8586eb4756f808fe7eb7442b884dc0527c67946f
Imane Chiki <imanechiki1994@gmail.com>
2024-09-18 18:07:52 +0100

    Implement the Optionals.

--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -4,26 +4,18 @@ import Modal from "../../components/modals/Modal";
 import Header from "../../components/Header/Header"
 import ExpensesList from "./ExpensesList";
 import Calculation from "./Calculation";
+import Optionals from "./Optionals";
 
 const Dashboard = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
-  // const handleClick = () => {
-  //   console.log("Button Clicked on Dashboard!");
-  // };
 
   return (
     <>
     <Header/>
     <ExpensesList/>
     <Calculation/>
-      {/* <StandardButton
-        label="New Expenses"
-        onClick={() => {
-          setModalVisible(true);
-        }}
-        customClass={'secondary'}
-      ></StandardButton>
-      <Modal visible={modalVisible} setVisible={setModalVisible}></Modal> */}
+    <Optionals/>
+      
     </>
   );
 };
